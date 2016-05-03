555083de0d4262762fc1ee25ffc777f423951d22
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-05-03 16:53:16 -0300

    Add Projects and Tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+
+  before_action :set_project, only: [:new, :create, :edit, :update, :destroy]
+
+  def new
+
+  end
+
+  def create
+    @task = @project.tasks.create(task_params)
+    redirect_to @project
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @task = @project.tasks.find(params[:id])
+    if @task.destroy
+      flash[:success] = "Task was deleted."
+    else
+      flash[:error] = "Task was not deleted"
+    end
+    redirect_to @project
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def task_params
+    params[:task].permit(:content)
+  end
+
+end
