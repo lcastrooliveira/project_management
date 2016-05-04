7d4421a93e585f33f7316b7e88946e8bfed323a6
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-05-03 23:21:16 -0300

    Mark as complete ability for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,17 @@
 class TasksController < ApplicationController
 
-  before_action :set_project, only: [:new, :create, :edit, :update, :destroy]
-
-  def new
-
-  end
+  before_action :set_project
+  before_action :set_task, except: [:create]
 
   def create
     @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
-  def edit
 
-  end
-
-  def update
-
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
   end
 
   def destroy
@@ -31,12 +26,16 @@ class TasksController < ApplicationController
 
   private
 
+  def set_task
+    @task = @project.tasks.find(params[:id])
+  end
+
   def set_project
     @project = Project.find(params[:project_id])
   end
 
   def task_params
-    params[:task].permit(:content)
+    @task = params[:task].permit(:content)
   end
 
 end
